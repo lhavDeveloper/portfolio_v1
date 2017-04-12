990951cdc3fcd10710132da8ca270bf0eb136b19
Lhavanya Murali <lhavanyamurali@Lhavanyas-MBP.home>
2017-04-12 15:06:18 -0400

    theme beautified

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 var age=23;
-var quotes="The future is a concept, it doesn’t exist﻿ There is no such thing as tomorrow. There never will be because time is always now. That’s one of the things we discover when we stop talking to ourselves and stop thinking. We find there is only present, only an eternal now.";
+var quotes="That’s been one of my mantras – focus and simplicity. Simple can be harder than complex: You have to work hard to get your thinking clean to make it simple. But it’s worth it in the end because once you get there, you can move mountains. - Steve Jobs";
 
 function init(){
 
@@ -8,23 +8,23 @@ function init(){
 var x=0;
 var parent=$('#hobbies');
 var currentWord=1;
-var intervalID = setInterval(function () {
-    $('#age').html(x);
-   if (x++ === age) {
-       window.clearInterval(intervalID);
-   }
-}, 50);
+// var intervalID = setInterval(function () {
+//     $('#age').html(x);
+//    if (x++ === age) {
+//        window.clearInterval(intervalID);
+//    }
+// }, 50);
 
-var intervalID2 = setInterval(function () {
+// var intervalID2 = setInterval(function () {
 
-var h=$('#hobbies li:nth-child('+currentWord+')');
-if(currentWord == $('#hobbies li').length)
-currentWord=0;
-var n=$('#hobbies li:nth-child('+ (++currentWord) +')');
-h.attr("class","invisible");
-n.attr("class","visible");
+// // var h=$('#hobbies li:nth-child('+currentWord+')');
+// // if(currentWord == $('#hobbies li').length)
+// // currentWord=0;
+// // var n=$('#hobbies li:nth-child('+ (++currentWord) +')');
+// // h.attr("class","invisible");
+// // n.attr("class","visible");
  
-}, 2000);
+// // }, 2000);
 var target=document.getElementById('quote');
 showText(target,quotes,0,50)
 }
