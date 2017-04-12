f8a897d3c045ac64dd5d2b7d9a3adb9392b2adaf
Lhavanya Murali <lhavanyamurali@Lhavanyas-MBP.home>
2017-04-12 08:59:01 -0400

    Final touches to layout of main page

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 
 var age=23;
+var quotes="The future is a concept, it doesn’t exist﻿ There is no such thing as tomorrow. There never will be because time is always now. That’s one of the things we discover when we stop talking to ourselves and stop thinking. We find there is only present, only an eternal now.";
 
 function init(){
 
 
-	var x=0;
-	var parent=$('#hobbies');
-	var currentWord=1;
+var x=0;
+var parent=$('#hobbies');
+var currentWord=1;
 var intervalID = setInterval(function () {
-   $('#age').html(x);
+    $('#age').html(x);
    if (x++ === age) {
        window.clearInterval(intervalID);
    }
@@ -24,5 +25,13 @@ h.attr("class","invisible");
 n.attr("class","visible");
  
 }, 2000);
+var target=document.getElementById('quote');
+showText(target,quotes,0,50)
+}
 
+function showText (target, message, index, interval) {  
+  if (index < message.length) {
+    $(target).append(message[index++]);
+    setTimeout(function () { showText(target, message, index, interval); }, interval);
+  }
 }
